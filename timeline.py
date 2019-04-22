216ceb91db6f176feeba6f5394d5de9e3a3fd455
liyarong <36287800+vigocat@users.noreply.github.com>
2019-04-22 22:50:38 +0800

    update
    添加读写文件函数

--- timeline.py
+++ timeline.py
@@ -18,10 +18,14 @@ print("               :2019end---")
 import random
 
 
-#axis:轴的位置 startstr:起始字符串
+#axis:轴的位置 startstr:起始字符串 data:数据
 axis = 20
 startstr = " "*(axis-5)+"---start2019:\n"+ " "*axis+"|"
 endstr =" "*(axis-4)+":2019end---"
+
+
+#data = [{'time':'06/32','something':'hhhhh'},\
+#        {'time':'02/14','something':'ok'}]
 
 
 def createNode(t,sth):
@@ -34,16 +38,57 @@ def createNode(t,sth):
     str1 = " "*(axis-5-r1)+t+'-'*r1+'|'
     str2 = " "*axis+"o"+"-"*r2+" "+sth
     str3 = " "*axis+"|"
-    return [str1,str2,str3]
+    return str1,str2,str3
 
 def printTimeline():
     '''
-       打印时间轴
+        打印时间轴
     '''
     print(startstr)
-    print(*createNode("06/32","hhhhh"),sep = "\n")
-    print(*createNode("02/14","成功了哈哈哈"),sep = "\n")
+    data = readData()
+    for item in data:
+        print(*createNode(item['time'],item['something']),sep='\n')
     print(endstr)
+
+
+def readData():
+    data = []
+    startflag = 0
+    with open('./timeline.py') as f:
+        for line in f:
+            if line.startswith("#data_start"):
+                startflag = 1
+                continue
+            if line.startswith("#data_end"):
+                break
+            if startflag:
+                if line[-1] == '\n':
+                    line = line[:-1]
+                if line[-1] == '\r':
+                    line = line[:-1]
+                data.append({'time':line[1:6],'something':line[6:]})
+    return data
+
+def writeData(t,sth):
+    lineStr = "#"+t+sth+' '*8 +'\n'
+    lineEnd = "#data_end"
+    with open("./timeline.py",'r+b') as f:
+        for line in f:
+            if line.startswith("#data_end".encode('utf-8')):
+                backstep = -len(line) 
+                f.seek(backstep,1)
+                f.write(lineStr.encode('utf-8'))
+                f.write(lineEnd.encode('utf-8'))
+                break
+    printTimeline()
+                
+
     
+
 printTimeline()
 
+
+#data_start
+#04/21linux开发环境搭建
+#04/22时间轴脚本                
+#data_end
